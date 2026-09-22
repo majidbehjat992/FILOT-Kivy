77961134df82234802f9090587993e58e3436d11
majidbehjat992 <44528987+majidbehjat992@users.noreply.github.com>
2026-09-22 19:02:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-__version__ = "1.0.0"
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+
+class FILOTApp(App):
+
+    def build(self):
+        layout = BoxLayout(
+            orientation="vertical",
+            padding=30,
+            spacing=20
+        )
+
+        title = Label(
+            text="FILOT Pump Assistant",
+            font_size=28
+        )
+
+        button = Button(
+            text="شروع"
+        )
+
+        layout.add_widget(title)
+        layout.add_widget(button)
+
+        return layout
+
+
+if __name__ == "__main__":
+    FILOTApp().run()__version__ = "1.0.0"
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
